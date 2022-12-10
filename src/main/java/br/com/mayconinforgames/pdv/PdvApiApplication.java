74becded2163b1b1d0217d30eb4e5ee8ab4a653f
maycon-santana <maycon.santana@thstec.com.br>
2022-12-10 15:21:54 -0300

    Iniciando as classe de Domain

--- src/main/java/br/com/mayconinforgames/pdv/PdvApiApplication.java
+++ src/main/java/br/com/mayconinforgames/pdv/PdvApiApplication.java
@@ -1,4 +1,4 @@
-package br.com.mayconinforgames.pdv.pdvapi;
+package br.com.mayconinforgames.pdv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/br/com/mayconinforgames/pdv/model/PessoaFisica.java
@@ -0,0 +1,82 @@
+package br.com.mayconinforgames.pdv.model;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_fisica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaFisica extends  Pessoa{
+
+    private String cnpj;
+    private String inscEstadual;
+    private String inscMunicipal;
+    private String nomeFantasia;
+    private String razaoSocial;
+    private String categoria;
+
+    public PessoaFisica() {
+    }
+
+    public PessoaFisica(Long id, String nome, String email, String senha, String cnpj,
+                        String inscEstadual, String inscMunicipal, String nomeFantasia,
+                        String razaoSocial, String categoria) {
+        super(id, nome, email, senha);
+        this.cnpj = cnpj;
+        this.inscEstadual = inscEstadual;
+        this.inscMunicipal = inscMunicipal;
+        this.nomeFantasia = nomeFantasia;
+        this.razaoSocial = razaoSocial;
+        this.categoria = categoria;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getInscEstadual() {
+        return inscEstadual;
+    }
+
+    public void setInscEstadual(String inscEstadual) {
+        this.inscEstadual = inscEstadual;
+    }
+
+    public String getInscMunicipal() {
+        return inscMunicipal;
+    }
+
+    public void setInscMunicipal(String inscMunicipal) {
+        this.inscMunicipal = inscMunicipal;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+}
