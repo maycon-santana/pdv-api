1153e02cddbea3918f59a37480487197aeb77d0b
maycon-santana <maycon.santana@thstec.com.br>
2022-12-10 14:15:09 -0300

    Projeto Criado

---
+++ src/test/java/br/com/mayconinforgames/pdv/pdvapi/PdvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mayconinforgames.pdv.pdvapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
