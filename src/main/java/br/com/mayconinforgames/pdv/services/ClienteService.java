1382cde899818fe03e03f56e0681383c57649573
maycon-santana <maycon.santana@thstec.com.br>
2022-12-11 19:32:08 -0300

    FindAll para Cliente

--- src/main/java/br/com/mayconinforgames/pdv/services/ClienteService.java
+++ src/main/java/br/com/mayconinforgames/pdv/services/ClienteService.java
@@ -6,6 +6,7 @@ import br.com.mayconinforgames.pdv.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +19,9 @@ public class ClienteService {
         Optional<Cliente> obj = clienteRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
     }
+
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
 
 }
