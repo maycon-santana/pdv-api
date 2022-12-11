f24728ba1ef91a354088666c591533e6df332b6b
maycon-santana <maycon.santana@thstec.com.br>
2022-12-11 20:41:37 -0300

    Create para Cliente e validação para CPF r RG já cadastrado

--- src/main/java/br/com/mayconinforgames/pdv/model/Cliente.java
+++ src/main/java/br/com/mayconinforgames/pdv/model/Cliente.java
@@ -9,6 +9,10 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.mayconinforgames.pdv.model.dtos.ClienteDTO;
+
 @Entity
 @Table(name = "cliente")
 @PrimaryKeyJoinColumn(name = "id")
@@ -16,10 +20,11 @@ public class Cliente extends Pessoa {
 
 	private static final long serialVersionUID = 1L;
 
-	@Column(nullable = false)
+	@CPF
+	@Column(unique = true)
 	private String cpf;
 
-	@Column(nullable = false)
+	@Column(unique = true)
 	private String rg;
 
 	@Temporal(TemporalType.DATE)
@@ -35,6 +40,22 @@ public class Cliente extends Pessoa {
 		this.cpf = cpf;
 		this.rg = rg;
 		this.dataNascimento = dataNascimento;
+	}
+
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+		this.telefone = obj.getTelefone();
+		this.logradouro = obj.getLogradouro();
+		this.cep = obj.getCep();
+		this.numero = obj.getNumero();
+		this.complemento = obj.getComplemento();
+		this.bairro = obj.getBairro();
+		this.uf = obj.getUf();
+		this.cidade = obj.getCidade();
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public String getCpf() {
--- src/main/java/br/com/mayconinforgames/pdv/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/mayconinforgames/pdv/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.mayconinforgames.pdv.resources.exceptions;
 
+import br.com.mayconinforgames.pdv.services.exceptions.DataIntegrityViolationException;
 import br.com.mayconinforgames.pdv.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,18 +12,30 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-    @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex,
-                                                                 HttpServletRequest request) {
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex,
+			HttpServletRequest request) {
 
-        StandardError error = new StandardError(
-                System.currentTimeMillis(),
-                HttpStatus.NOT_FOUND.value(),
-                "Object Not Found",
-                ex.getMessage(), request.getRequestURI());
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+				"Object Not Found", ex.getMessage(), request.getRequestURI());
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 
-    }
+	}
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+			HttpServletRequest request) {
+
+		StandardError error = new StandardError(
+				System.currentTimeMillis(), 
+				HttpStatus.BAD_REQUEST.value(),
+				"Violação de dados", 
+				ex.getMessage(), 
+				request.getRequestURI());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+
+	}
 
 }
