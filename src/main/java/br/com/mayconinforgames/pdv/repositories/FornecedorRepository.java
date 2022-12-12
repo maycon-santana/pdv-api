9f312eaa9182b86023a75c8d3076079b657f37a6
maycon-santana <maycon.santana@thstec.com.br>
2022-12-11 22:13:45 -0300

    Create para Fornecedores

--- src/main/java/br/com/mayconinforgames/pdv/repositories/FornecedorRepository.java
+++ src/main/java/br/com/mayconinforgames/pdv/repositories/FornecedorRepository.java
@@ -1,7 +1,13 @@
 package br.com.mayconinforgames.pdv.repositories;
 
-import br.com.mayconinforgames.pdv.model.Fornecedor;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.mayconinforgames.pdv.model.Fornecedor;
+
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+
+	Optional<Fornecedor> findByCnpj(String cnpj);
+	
 }
